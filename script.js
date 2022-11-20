b012f197e6ccc33e5b9b336d76f39a3d2bd6ce94
iniruimar <73244791+iniruimar@users.noreply.github.com>
2022-11-20 12:50:18 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let person = "Iñigo";
+let edad = 20;
+console.log(person, edad);
+mas_edad = edad*7;
+console.log(mas_edad);
+let amigos = ["Javier", "Guillermo", "Pedro"]
+console.log(amigos);
+let students =[
+    {
+        nombre: "Javier",
+        edad: 20,
+        isStudent: true
+    },
+    {
+        nombre: "Guille",
+        edad: 20,
+        isStudent: false
+    },
+    {
+        nombre: "Pedro",
+        edad: 20,
+        isStudent: true
+    }
+];
+console.log(students)
